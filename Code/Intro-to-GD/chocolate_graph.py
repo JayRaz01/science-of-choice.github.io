7f3a65557315447dc0438dd8e1d5dfd3ac723282
JayRaz01 <jayrazdan@yahoo.com>
2024-08-16 18:12:23 +0100

    Create chocolate_graph.py

---
+++ Code/Intro-to-GD/chocolate_graph.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0.01, 10, 500)  # x ranges from 0.01 to 10, with 500 points
+y = np.log(x) + 4.5 # The natural logarithm of x
+
+plt.plot(x, y)
+plt.title("A plot of Bob's utility")
+plt.xlabel("Chunks of chocolate eaten")
+plt.ylabel("Utility")
+plt.grid(True)
+plt.axhline(0, color='black',linewidth=0.5)
+plt.axvline(0, color='black',linewidth=0.5)
+plt.legend()
+plt.show()
